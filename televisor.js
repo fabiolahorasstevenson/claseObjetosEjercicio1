3a961f87d96520f37d023b1ef379d736bf2b6e03
irinamarzioni <imarzioni@devartis.com>
2022-09-15 14:37:22 -0300

    correccion

--- televisor.js
+++ televisor.js
@@ -1,38 +1,38 @@
-var decodificador = /** @class */ (function () {
-    function decodificador(paramModelo, paramMarca) {
+var Decodificador = /** @class */ (function () {
+    function Decodificador(paramModelo, paramMarca) {
         this.marca = paramMarca;
         this.modelo = paramModelo;
     }
-    return decodificador;
+    return Decodificador;
 }());
-var televisor = /** @class */ (function () {
-    function televisor(paramTamaño, paramEsSmart, paramEstaPrendido, paramVolumen, paramDecodificador) {
+var Televisor = /** @class */ (function () {
+    function Televisor(paramTamaño, paramEsSmart, paramEstaPrendido, paramVolumen, paramDecodificador) {
         this.tamaño = paramTamaño;
         this.esSmart = paramEsSmart;
         this.estaPrendido = paramEstaPrendido;
         this.volumen = paramVolumen;
         this.decodificador = paramDecodificador;
     }
-    televisor.prototype.getTamaño = function () {
+    Televisor.prototype.getTamaño = function () {
         return this.tamaño;
     };
-    televisor.prototype.setTamaño = function (paramTamaño) {
+    Televisor.prototype.setTamaño = function (paramTamaño) {
         this.tamaño = paramTamaño;
     };
-    televisor.prototype.getEsSmart = function () {
+    Televisor.prototype.getEsSmart = function () {
         return this.esSmart;
     };
-    televisor.prototype.setEsSmart = function (paramEsSmart) {
+    Televisor.prototype.setEsSmart = function (paramEsSmart) {
         this.esSmart = paramEsSmart;
     };
-    televisor.prototype.apagar = function () {
+    Televisor.prototype.apagar = function () {
         this.estaPrendido = false;
     };
-    televisor.prototype.encender = function () {
+    Televisor.prototype.encender = function () {
         this.estaPrendido = true;
     };
-    return televisor;
+    return Televisor;
 }());
-var deco1 = new decodificador("Deco", "Deco");
-var televisor1 = new televisor(100, true, true, 20, deco1);
+var deco1 = new Decodificador("Deco", "Deco");
+var televisor1 = new Televisor(100, true, true, 20, deco1);
 console.log(televisor1);
